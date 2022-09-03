c5d1746719f6c955ae2e7bb803b571d073bad6c1
C3B4 <90920026+C3B4@users.noreply.github.com>
2022-09-03 22:04:46 +0200

    1.1
    English version.

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,24 +1,24 @@
 import random
 import os
 
-dlugosc = input("Jaka ma być długość hasla: ")
-ilosc = input("Jaka ma być ilość haseł: ")
-znaki = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+leng = input("How long passwords should be: ")
+count = input("What count of passwords should be: ")
+characters = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
 password = ""
 
-if dlugosc == "":
-    dlugosc = 16
-if ilosc == "":
-    ilosc = 1
+if leng == "":
+    leng = 16
+if count == "":
+    count = 1
 
-if int(dlugosc) < 1:
-   print("Długość hasła nie może być któtsza niż 1")
-if int(ilosc) < 1:
-    print("Ilość haseł nie może być mniejsza niż 1")
+if int(leng) < 1:
+   print("Passwords leng can't be smaller than 1")
+if int(count) < 1:
+    print("Passwords count can't be smaller than 1")
 
-for i in range(int(ilosc)):
-    for x in range(int(dlugosc)):
-        z = random.choice(znaki)
+for i in range(int(count)):
+    for x in range(int(leng)):
+        z = random.choice(characters)
         password += z
     print(password)
     password = ""
